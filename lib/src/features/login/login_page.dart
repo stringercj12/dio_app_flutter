ab548ecdc5b3f5eb49d704868fd594a62e0105af
Jefferson Ferreira <jefferson14489@gmail.com>
2023-09-07 21:15:32 -0300

    conhecendo o TextButton

---
+++ lib/src/features/login/login_page.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            Row(
+              children: [
+                Expanded(child: Container()),
+                Expanded(
+                  flex: 8,
+                  child: Image.network(
+                    "https://hermes.digitalinnovation.one/assets/diome/logo.png",
+                    errorBuilder: (BuildContext context, Object exception,
+                        StackTrace? stackTrace) {
+                      return const Text('😢');
+                    },
+                  ),
+                ),
+                Expanded(child: Container()),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Já tem cadastro?',
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.w700,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              'Faça seu login e make the change_',
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.white,
+              ),
+            ),
+            Container(
+              width: double.infinity,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: TextButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  backgroundColor: MaterialStateProperty.all(
+                    const Color.fromARGB(255, 90, 16, 101),
+                  ),
+                ),
+                child: const Text(
+                  'Entrar',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(child: Container()),
+            Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 30,
+              ),
+              height: 30,
+              alignment: Alignment.center,
+              child: const Text(
+                'Esqueci minha senha',
+                style: TextStyle(
+                  color: Colors.yellow,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 30,
+              ),
+              height: 30,
+              alignment: Alignment.center,
+              child: const Text(
+                'Criar conta',
+                style: TextStyle(
+                  color: Colors.green,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
