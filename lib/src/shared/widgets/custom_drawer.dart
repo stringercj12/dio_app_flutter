2cbc3fc615c4f07ab8209c458d2c87da645c2131
Jefferson Ferreira <jefferson14489@gmail.com>
2023-09-21 14:28:45 -0300

    showAlertDialog e showModalBottomSheet

--- lib/src/shared/widgets/custom_drawer.dart
+++ lib/src/shared/widgets/custom_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:dio_app_flutter/src/features/dados_cadastrais/dados_cadastrais_page.dart';
+import 'package:dio_app_flutter/src/features/login/login_page.dart';
 import 'package:flutter/material.dart';
 
 class CustomDrawer extends StatelessWidget {
@@ -10,16 +11,45 @@ class CustomDrawer extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const UserAccountsDrawerHeader(
-            decoration: BoxDecoration(color: Colors.orange),
-            currentAccountPicture: CircleAvatar(
-              backgroundColor: Colors.white,
-              backgroundImage: NetworkImage(
-                "https://images.unsplash.com/photo-1694309984301-60e69e095ae7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60",
+          InkWell(
+            onTap: () {
+              showModalBottomSheet(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  context: context,
+                  builder: (BuildContext buildContext) {
+                    return Wrap(
+                      children: [
+                        ListTile(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          title: const Text('Câmera'),
+                          leading: const Icon(Icons.camera),
+                        ),
+                        ListTile(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          title: const Text('Galeria'),
+                          leading: const Icon(Icons.photo_album),
+                        ),
+                      ],
+                    );
+                  });
+            },
+            child: const UserAccountsDrawerHeader(
+              decoration: BoxDecoration(color: Colors.orange),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                backgroundImage: NetworkImage(
+                  "https://images.unsplash.com/photo-1694309984301-60e69e095ae7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60",
+                ),
               ),
+              accountName: Text('Jefferson Ferreira'),
+              accountEmail: Text('jefferson14489@gmail.com'),
             ),
-            accountName: Text('Jefferson Ferreira'),
-            accountEmail: Text('jefferson14489@gmail.com'),
           ),
           InkWell(
             child: Container(
@@ -60,7 +90,39 @@ class CustomDrawer extends StatelessWidget {
                 ],
               ),
             ),
-            onTap: () {},
+            onTap: () {
+              showModalBottomSheet(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                context: context,
+                builder: (BuildContext buildContext) {
+                  return Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 12),
+                    child: const Column(
+                      children: [
+                        Text(
+                          'Termos de uso e privacidade',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(fontSize: 14),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            },
           ),
           const Divider(),
           InkWell(
@@ -78,6 +140,68 @@ class CustomDrawer extends StatelessWidget {
               ),
             ),
             onTap: () {},
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              width: double.infinity,
+              child: const Row(
+                children: [
+                  Icon(Icons.exit_to_app),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text('Sair'),
+                ],
+              ),
+            ),
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext buildContext) {
+                  return AlertDialog(
+                    alignment: Alignment.centerLeft,
+                    elevation: 8,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    title: const Text(
+                      'Meu app',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    content: const Wrap(
+                      children: [
+                        Text('Você sairá do aplicativo!'),
+                        Text('Deseja realmente sair do aplicativo?'),
+                      ],
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text('Não'),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: ((context) => const LoginPage()),
+                            ),
+                          );
+                        },
+                        child: const Text('Sim'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
           ),
         ],
       ),
