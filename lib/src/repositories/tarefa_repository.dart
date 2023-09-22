e5c501937a42dfb9750bd333da42187f408ccbb7
Jefferson Ferreira <jefferson14489@gmail.com>
2023-09-22 12:37:45 -0300

    Lista de Tarefas - Criando a Model e a Repositor

---
+++ lib/src/repositories/tarefa_repository.dart
@@ -0,0 +1,22 @@
+import 'package:dio_app_flutter/src/model/tarefa.dart';
+
+class TarefaRepository {
+  final List<Tarefa> _tarefas = [];
+
+  void adicionar(Tarefa tarefa) {
+    _tarefas.add(tarefa);
+  }
+
+  void alterar(String id, bool concluido) async {
+    await Future.delayed(const Duration(seconds: 1));
+
+    var tarefa = _tarefas.where((tarefa) => tarefa.getId() == id).first;
+
+    tarefa.setConcluido(concluido);
+  }
+
+  Future<List<Tarefa>> listarTarefas() async {
+    await Future.delayed(const Duration(seconds: 1));
+    return _tarefas;
+  }
+}
