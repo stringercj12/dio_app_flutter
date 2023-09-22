0aad7f2982cc52a53bf88d16c350401939af079b
Jefferson Ferreira <jefferson14489@gmail.com>
2023-09-22 15:39:04 -0300

    Lista de Tarefas - Removendo Itens

--- lib/src/pages/tarefa_page.dart
+++ lib/src/pages/tarefa_page.dart
@@ -13,16 +13,22 @@ class _TarefaPageState extends State<TarefaPage> {
   var tarefaRepository = TarefaRepository();
   var _tarefas = const <Tarefa>[];
   var descricaoController = TextEditingController();
+  var apenasNaoConcluidos = false;
 
   void obterTarefas() async {
-    _tarefas = await tarefaRepository.listarTarefas();
+    if (apenasNaoConcluidos) {
+      _tarefas = await tarefaRepository.listarNaoConcluida();
+    } else {
+      _tarefas = await tarefaRepository.listarTarefas();
+    }
+    setState(() {});
   }
 
   @override
   void initState() {
     // TODO: implement initState
-    super.initState();
     obterTarefas();
+    super.initState();
   }
 
   @override
@@ -54,13 +60,16 @@ class _TarefaPageState extends State<TarefaPage> {
                       debugPrint(descricaoController.text);
                       tarefaRepository
                           .adicionar(Tarefa(descricaoController.text, false));
+                      Navigator.pop(context);
                       setState(() {});
+                      obterTarefas();
+
                       ScaffoldMessenger.of(context).showSnackBar(
                         const SnackBar(
                           content: Text('Tarefa adicionada'),
+                          duration: Duration(seconds: 200),
                         ),
                       );
-                      Navigator.pop(context);
                     },
                     child: const Text('Salvar'),
                   ),
@@ -71,12 +80,53 @@ class _TarefaPageState extends State<TarefaPage> {
         },
         child: const Icon(Icons.add),
       ),
-      body: ListView.builder(
-        itemCount: _tarefas.length,
-        itemBuilder: (BuildContext buildContext, int index) {
-          var tarefa = _tarefas[index];
-          return Text(tarefa.getDescricao());
-        },
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text('Filtrar apenas não concluídos'),
+                  Switch(
+                    value: apenasNaoConcluidos,
+                    onChanged: (bool value) {
+                      apenasNaoConcluidos = value;
+                      obterTarefas();
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _tarefas.length,
+                itemBuilder: (BuildContext buildContext, int index) {
+                  var tarefa = _tarefas[index];
+                  return Dismissible(
+                    onDismissed: (DismissDirection dismissDirection) async {
+                      await tarefaRepository.remover(tarefa.getId());
+                      obterTarefas();
+                    },
+                    key: Key(tarefa.getId()),
+                    child: ListTile(
+                      title: Text(tarefa.getDescricao()),
+                      trailing: Switch(
+                        onChanged: (bool value) async {
+                          await tarefaRepository.alterar(tarefa.getId(), value);
+                          obterTarefas();
+                        },
+                        value: tarefa.getConcluido(),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/src/repositories/tarefa_repository.dart
+++ lib/src/repositories/tarefa_repository.dart
@@ -4,20 +4,32 @@ class TarefaRepository {
   final List<Tarefa> _tarefas = [];
 
   Future<void> adicionar(Tarefa tarefa) async {
-    await Future.delayed(const Duration(seconds: 1));
+    await Future.delayed(const Duration(milliseconds: 100));
     _tarefas.add(tarefa);
   }
 
-  void alterar(String id, bool concluido) async {
-    await Future.delayed(const Duration(seconds: 1));
+  Future<void> alterar(String id, bool concluido) async {
+    await Future.delayed(const Duration(milliseconds: 100));
 
     var tarefa = _tarefas.where((tarefa) => tarefa.getId() == id).first;
 
     tarefa.setConcluido(concluido);
   }
 
+  Future<void> remover(String id) async {
+    await Future.delayed(const Duration(milliseconds: 100));
+
+    var tarefa = _tarefas.where((tarefa) => tarefa.getId() == id).first;
+    _tarefas.remove(tarefa);
+  }
+
   Future<List<Tarefa>> listarTarefas() async {
-    await Future.delayed(const Duration(seconds: 1));
+    await Future.delayed(const Duration(milliseconds: 100));
     return _tarefas;
+  }
+
+  Future<List<Tarefa>> listarNaoConcluida() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    return _tarefas.where((tarefa) => !tarefa.getConcluido()).toList();
   }
 }
